550c93f6c0604bfb022fcda25174bba98f8856d8
elektrikmusik <elektrikmusik@gmail.com>
2025-11-02 08:22:56 +0800

    test: verify pre-commit hook

---
+++ test-husky-temp.md
@@ -0,0 +1,7 @@
+# Test File
+
+This file has formatting issues that prettier should fix
+
+- Bad spacing
+- Inconsistent spacing
+  -No space before dash
---
+++ test-husky-temp.ts
@@ -0,0 +1,9 @@
+// Test file with intentional formatting issues (all fixable)
+const testVar = 'unformatted';
+
+function testFunction() {
+  console.log('bad formatting');
+  return testVar;
+}
+
+export default testFunction;
